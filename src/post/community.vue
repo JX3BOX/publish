b3d4356a7743b3be62b386704dddf75859cda988
糖醋虾仁 <liuchuana@hotmail.com>
2024-06-13 00:30:32 +0800

    fix 修复论坛摩卡 展示

--- src/post/community.vue
+++ src/post/community.vue
@@ -400,9 +400,8 @@ export default {
         padding-bottom: 20px;
         .u-imgs-item {
             position: relative;
-            min-width: 150px;
-            width: 350px;
-            height: 100px;
+            min-width: 160px;
+            height: 50px;
             box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
             .el-image {
                 width: 100%;
